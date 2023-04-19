8f9dfa568fb771d12edd2c605314415c2ddbb3c3
a161120 <david.tarvin@physiciansmutual.com>
2023-04-18 20:56:29 -0500

    first commit

---
+++ src/test/java/com/example/buildingrest/BuildingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buildingrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
